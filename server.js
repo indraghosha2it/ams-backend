db7b88850efb2f8309ec8336b40912b1d1e6f654
indraghosha2it <indraghosh.a2it@gmail.com>
2026-02-08 11:03:06 +0600

    netlify

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000', 
     'http://localhost:5000',
-    'https://bespoke-lamington-995d09.netlify.app'
+    'https://marvelous-kataifi-a31d34.netlify.app',
    
     ],
   credentials: true,
